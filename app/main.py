352c97d7e7493ec0289564062468c88b90594c7d
Bartek <bartlomiejadamiec65@gmail.com>
2025-12-07 15:17:55 +0100

    Pierwszy endpoint, przesłany plik zapisuje się na dysku

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-
-app = FastAPI()
-
+from app.routers import documents
+app = FastAPI(title="File Cert API")
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "System File Cert działa poprawnie"}
+
+app.include_router(documents.router, prefix="/api", tags=["documents"])
--- app/routers/documents.py
+++ app/routers/documents.py
@@ -1,4 +1,17 @@
 # Obsługa plików (/upload, /sign, /verify)
-
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File, HTTPException
+import os
+import shutil
 router = APIRouter()
+
+@router.post("/upload")
+async def upload_file(file: UploadFile = File()):
+    if not file.filename.endswith(".pdf"): # Walidacja czy to plik .pdf
+        raise HTTPException(status_code=400, detail="Invalid extension. Must be .pdf file")
+
+    save_path = f"storage/{file.filename}"
+    os.makedirs("storage", exist_ok=True) # Upewnienie czy folder istnieje
+
+    with open(save_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return file.filename
