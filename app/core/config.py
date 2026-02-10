53c6d9d839cf58eaec5328927cff0e9c22bb4da0
Bartek <bartlomiejadamiec65@gmail.com>
2026-02-10 20:12:26 +0100

    minor changes in config

--- app/core/config.py
+++ app/core/config.py
@@ -4,11 +4,14 @@ import os
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ROOT_CA_PASSWORD = os.getenv("ROOT_CA_PASSWORD")
-ALGORITHM = "HS256"
+ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = 15
-CERTS_DIR = "app/certs"
-ROOT_CA_KEY_PATH = os.path.join(CERTS_DIR, "root_ca.key")
-ROOT_CA_CERT_PATH = os.path.join(CERTS_DIR, "root_ca.crt")
-MONGO_PATH = "mongodb://localhost:27017/"
-STORAGE = "./storage"
+CERTS_DIR = "CERTS_DIR"
+ROOT_CA_KEY = str(os.getenv("ROOT_CA_KEY"))
+ROOT_CA_KEY_PATH = os.path.join(CERTS_DIR, ROOT_CA_KEY)
+ROOT_CA_CERT = str(os.getenv("ROOT_CA_CERT"))
+ROOT_CA_CERT_PATH = os.path.join(CERTS_DIR, ROOT_CA_CERT)
+MONGO_PATH = os.getenv("MONGO_PATH")
+DB_NAME = os.getenv("DB_NAME")
+STORAGE = os.getenv("STORAGE")
 OAUTH2_SCHEME = OAuth2PasswordBearer(tokenUrl="token")
